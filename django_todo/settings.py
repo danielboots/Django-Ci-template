0cb41ea47a87fcffbdae2e15cebf7f7d1bd3da4f
Dan Boots <thestereojackers@gmail.com>
2021-01-21 16:12:27 +0000

    set up de env

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,10 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
+if development:
+    ALLOWED_HOSTS = ["localhost"]
+else:
+    ALLOWED_HOSTS = [os.environ.get("HEROKU_HOSTNAME")]
 
 
 # Application definition
